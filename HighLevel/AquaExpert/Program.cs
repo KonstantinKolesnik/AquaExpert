4d713135e8cfbab8e2257f78abf07c501198c0b4
1 <1>
2014-02-17 01:25:40 +0200

    in progress

--- HighLevel/AquaExpert/Program.cs
+++ HighLevel/AquaExpert/Program.cs
@@ -1,6 +1,10 @@
 ﻿using BusNetwork.Network;
 using Gadgeteer.Modules.KKSolutions;
 using GHI.Premium.System;
+using MFE.Graphics;
+using MFE.Graphics.Controls;
+using MFE.Graphics.Geometry;
+using MFE.Graphics.Media;
 using MFE.Net.Http;
 using MFE.Net.Managers;
 using MFE.Net.Messaging;
@@ -100,9 +104,9 @@ namespace AquaExpert
 
             networkCoordinator = new NetworkCoordinator();
 
-            busHubI2C = new BusHubI2C(new BusConfiguration(new I2CDevice(null)));
-            busHubI2C.BusModulesCollectionChanged += busHubI2C_BusModulesCollectionChanged;
-            networkCoordinator.BusHubs.Add(busHubI2C);
+            //busHubI2C = new BusHubI2C(new BusConfiguration(new I2CDevice(null)));
+            //busHubI2C.BusModulesCollectionChanged += busHubI2C_BusModulesCollectionChanged;
+            //networkCoordinator.BusHubs.Add(busHubI2C);
         }
         private void InitHardware()
         {
@@ -131,10 +135,10 @@ namespace AquaExpert
             display = new Display_SP22(1);
 
             // Usage example #1. Passing a Bitmap to the driver.
-            Bitmap picture = new Bitmap(Resources.GetBytes(Resources.BinaryResources.test_24b), Bitmap.BitmapImageType.Bmp);
-            //display.Draw(picture);
+            //Bitmap bitmap = new Bitmap(Resources.GetBytes(Resources.BinaryResources.test_24b), Bitmap.BitmapImageType.Bmp);
+            //display.Draw(bitmap);
 
-            //display.SimpleGraphics.DisplayImage(picture, 0, 0);
+            //display.SimpleGraphics.DisplayImage(bitmap, 0, 0);
             ////display.SimpleGraphics.BackgroundColor = GT.Color.Green;
             //display.SimpleGraphics.DisplayText("Igor, mi bogati!", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.Red, 5, 5);
             //display.SimpleGraphics.DisplayText("Pivo v studiyu!", Resources.GetFont(Resources.FontResources.small), GT.Color.Red, 5, 25);
@@ -144,6 +148,22 @@ namespace AquaExpert
             //Thread.Sleep(2000);
 
             //DisplayDemo(display);
+
+
+            display.SimpleGraphics.Clear();
+
+            Panel pnl = new Panel(0, 0, 150, 180);
+            //pnl.Background = new LinearGradientBrush(Color.Blue, Color.Red);
+            pnl.Background = new SolidColorBrush(Color.Blue);
+
+            GraphicsManager.Initialize(240, 320);
+            GraphicsManager.OnRender += delegate(Bitmap bitmap, Rect dirtyArea)
+            {
+                display.SimpleGraphics.DisplayImage(bitmap, (uint)dirtyArea.X, (uint)dirtyArea.Y, (uint)dirtyArea.X, (uint)dirtyArea.Y, (uint)dirtyArea.Width, (uint)dirtyArea.Height);
+            };
+
+            GraphicsManager.Content = pnl;
+            CrashTest(pnl);
         }
         void DisplayDemo(Display_SP22 display)
         {
@@ -438,6 +458,271 @@ namespace AquaExpert
 
             Thread.Sleep(10000);
         }
+        private void CrashTest(Panel pnl)
+        {
+            //Width = 800;
+            //Height = 480;
+
+            //Width = 250;
+            //Height = 480;
+
+            font = Resources.GetFont(Resources.FontResources.small);
+
+            pnl.SuspendLayout();
+            for (int i = 0; i < 10; i++)
+                pnl.Children.Add(new TextBlock(10, 15 * i, 140, 20, font, "label" + i.ToString()) { ForeColor = Color.Brown });
+            pnl.ResumeLayout();
+
+            new Thread(delegate()
+            {
+                //int n = 0;
+                while (true)
+                {
+                    //Debug.Print("--------- Enter loop");
+
+                    DateTime dt = DateTime.Now;
+                    int c = pnl.Children.Count;
+                    for (int j = 0; j < c; j++)
+                    {
+                        //dt0 = DateTime.Now;
+
+
+
+                        //Label lbl = (Label)Children.GetAt(j);
+                        TextBlock lbl = (TextBlock)pnl.Children[j];
+                        //Debug.Print("Label text set");
+                        lbl.Text = dt.Ticks.ToString();
+                        //Thread.Sleep(10);
+
+                        //ts = DateTime.Now - dt0;
+                        //n++;
+                        //sum += ts;
+
+                        //Debug.Print("MFE: " + ts.ToString());
+                        //Debug.Print("MFE (average): " + new TimeSpan(sum.Ticks / n).ToString());
+
+                    }
+                    //Thread.Sleep(1000);
+                }
+            }
+            ).Start();
+
+            //Thread.Sleep(100);
+
+            //new Thread(delegate()
+            //{
+            //    while (true)
+            //    {
+            //        pnl.Translate(1, 1);
+
+            //        if (pnl.X >= display.Width - 10)
+            //            pnl.X = 0;
+            //        if (pnl.Y >= display.Height - 10)
+            //            pnl.Y = 0;
+
+            //        Thread.Sleep(1000);
+            //    }
+            //}
+            //).Start();
+        }
+        private void UIDemo(Panel pnl)
+        {
+            //Width = LCDManager.ScreenWidth;
+            //Height = LCDManager.ScreenHeight;
+            //Width = 320;
+            //Height = 240;
+
+
+
+        //    int k = Height / 240;
+        //    font = Resources.GetFont(Resources.FontResources.CourierNew_10);
+
+        //    ImageBrush brush = new ImageBrush(Resources.GetBitmap(Resources.BinaryResources.Background_800_600));
+        //    brush.Stretch = Stretch.Fill;
+        //    Background = brush;
+
+        //    int statusbarHeight = 24;
+        //    Panel statusbar = new Panel(0, Height - statusbarHeight, Width, statusbarHeight);
+        //    statusbar.Background = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Bar));
+        //    Children.Add(statusbar);
+
+        //    Label lblClock = new Label(statusbar.Width - 70, 4, font, "00:00:00");
+        //    lblClock.ForeColor = Color.White;
+        //    statusbar.Children.Add(lblClock);
+
+        //    Level lvl2 = new Level(statusbar.Width - 120, 7, 40, 10, Orientation.Horizontal, 10);
+        //    lvl2.Foreground = new LinearGradientBrush(Color.LimeGreen, Color.Black);
+        //    //lvl2.Value = 50;
+        //    statusbar.Children.Add(lvl2);
+
+        //    statusbar.Children.Add(new Image(statusbar.Width - 160, 1, 23, 23, Resources.GetBitmap(Resources.BitmapResources.Drive)));
+        //    statusbar.Children.Add(new Image(statusbar.Width - 185, 1, 23, 23, Resources.GetBitmap(Resources.BitmapResources.Mouse)));
+        //    //statusbar.Children.Add(new Image(statusbar.Width - 210, 1, 23, 23, Resources.GetBitmap(Resources.BitmapResources.Keyboard)));
+
+        //    //ToolButton btnHome = new ToolButton(10, 0, 70, statusbar.Height);
+        //    Button btnHome = new Button(10, 0, 70, statusbar.Height, null, "", Color.Black);
+        //    btnHome.Foreground = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Home));
+        //    btnHome.Border = null;
+        //    //btnHome.Enabled = false;
+        //    statusbar.Children.Add(btnHome);
+
+        //    //-------------------------------
+
+        //    //Children.Add(new Checkbox(20*k, 20*k, 20*k, 20*k));
+        //    Children.Add(new Checkbox(20, 20, 20, 20));
+
+
+        //    //return;
+        //    Children.Add(new TextBlock(500, 10, 100, 100, font, "Hello world! I'm a text block. I'm very cool!")
+        //    {
+        //        ForeColor = Color.White,
+        //        Background = new LinearGradientBrush(Color.Aquamarine, Color.Yellow) { Opacity = 100 },
+        //        TextAlignment = TextAlignment.Center,
+        //        TextVerticalAlignment = VerticalAlignment.Top,
+        //        TextWrap = true
+
+        //    });
+
+
+        //    Level lvl = new Level(20, 40, 60, 20, Orientation.Horizontal, 10);
+        //    lvl.Foreground = new LinearGradientBrush(Color.Blue, Color.Black);
+        //    //lvl.Value = 0;
+        //    Children.Add(lvl);
+
+
+
+
+        //    ProgressBar pg = new ProgressBar(20, 80, 100, 10);
+        //    pg.Foreground = new LinearGradientBrush(Color.LimeGreen, Color.Red);
+        //    //pg.Foreground.Opacity = 220;
+        //    Children.Add(pg);
+
+        //    Panel pnl = new Panel(20, 100, 100, 100);
+        //    pnl.Background = new LinearGradientBrush(Color.Blue, Color.LimeGreen);
+        //    //pnl.Background.Opacity = 80;
+        //    Children.Add(pnl);
+
+        //    Button btn = new Button(20, 220, 80, 30, font, "<", Color.White);
+        //    Children.Add(btn);
+
+        //    Button btn2 = new Button(60, 0, 80, 25, font, "Button 2 wwwwwwww", Color.White)
+        //    {
+        //        //BackgroundUnpressed = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.ButtonBackground)) { Opacity = 100 };
+        //        //BackgroundPressed = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.ButtonBackground)) { Opacity = 220 };
+        //    };
+        //    btn2.Click += delegate(object sender, EventArgs e)
+        //    {
+        //        wndModal dlg = new wndModal(0, 0, 0, 0);
+        //        dlg.ShowModal();
+
+        //        int a = 0;
+        //        int b = a;
+
+        //        //Close();
+        //    };
+        //    statusbar.Children.Add(btn2);
+
+        //    RadioButtonGroup rbg = new RadioButtonGroup(20, 260, 25, 70);
+        //    rbg.Background = new LinearGradientBrush(Color.White, Color.DarkGray);
+        //    //rbg.Background.Opacity = 120;
+        //    rbg.AddRadioButton(new RadioButton(5, 5, 15, true));
+        //    rbg.AddRadioButton(new RadioButton(5, 25, 15));
+        //    rbg.AddRadioButton(new RadioButton(5, 45, 15));
+        //    Children.Add(rbg);
+
+
+        //    ToolButton tbtn;
+
+        //    tbtn = new ToolButton(300, 150, 128, 128);
+        //    //tbtn.BackgroundUnpressed = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.ButtonBackground)) { Opacity = 100 };
+        //    //tbtn.BackgroundPressed = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.ButtonBackground)) { Opacity = 220 };
+        //    tbtn.Foreground = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Database));
+        //    tbtn.Foreground.Opacity = 200;
+        //    Children.Add(tbtn);
+
+        //    tbtn = new ToolButton(450, 150, 128, 128);
+        //    tbtn.Foreground = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Operation));
+        //    tbtn.Foreground.Opacity = 200;
+        //    Children.Add(tbtn);
+
+        //    tbtn = new ToolButton(600, 150, 128, 128);
+        //    tbtn.Foreground = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Settings));
+        //    tbtn.Foreground.Opacity = 200;
+        //    Children.Add(tbtn);
+
+        //    Children.Add(new Slider(250, 20, 150, 30, 15, Orientation.Horizontal)
+        //    {
+        //        Value = 80,
+        //        Background = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Bar)),
+        //        Foreground = new LinearGradientBrush(Color.LightGray, Color.Black) { Opacity = 50 }
+        //    });
+        //    Children.Add(new Slider(200, 20, 30, 150, 12, Orientation.Vertical)
+        //    {
+        //        Value = 70,
+        //        Background = new SolidColorBrush(Color.White) { Opacity = 100 }
+        //    });
+
+        //    Slider slider = new Slider(250, 60, 150, 30, 30, Orientation.Horizontal)
+        //    {
+        //        Value = 80,
+        //        Background = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.Bar)),
+        //        Foreground = new ImageBrush(Resources.GetBitmap(Resources.BitmapResources.GHILogo)) { Opacity = 200 },
+        //    };
+        //    Children.Add(slider);
+        //    Label lbl = new Label(250, 100, font, slider.Value.ToString()) { ForeColor = Color.White };
+        //    Children.Add(lbl);
+        //    slider.ValueChanged += delegate(object sender, ValueChangedEventArgs e) { lbl.Text = e.Value.ToString(); };
+
+        //    new Thread(delegate()
+        //    {
+        //        int v = 0;
+        //        while (true)
+        //        {
+        //            DateTime dt = DateTime.Now;
+
+        //            string hour = (dt.Hour < 10) ? "0" + dt.Hour.ToString() : dt.Hour.ToString();
+        //            string minute = (dt.Minute < 10) ? "0" + dt.Minute.ToString() : dt.Minute.ToString();
+        //            string second = (dt.Second < 10) ? "0" + dt.Second.ToString() : dt.Second.ToString();
+        //            string result = hour + ":" + minute + ":" + second;
+        //            lblClock.Text = result;
+
+        //            v += 10;
+        //            if (v > 100)
+        //                v = 0;
+
+        //            lvl.Value = v;
+        //            pg.Value = v;
+        //            lvl2.Value = v;
+
+        //            //Color temp = ((LinearGradientBrush)pnl.Background).StartColor;
+        //            //((LinearGradientBrush)pnl.Background).StartColor = ((LinearGradientBrush)pnl.Background).EndColor;
+        //            //((LinearGradientBrush)pnl.Background).EndColor = temp;
+        //            //pnl.Invalidate();
+
+        //            Thread.Sleep(500);
+        //        }
+        //    }).Start();
+
+        //    //new Thread(delegate()
+        //    //{
+        //    //    while (true)
+        //    //    {
+        //    //        Translate(1, 1);
+
+        //    //        if (X >= LCDManager.ScreenWidth - 100)
+        //    //            X = 0;
+        //    //        if (Y >= LCDManager.ScreenHeight - 100)
+        //    //            Y = 0;
+
+        //    //        Thread.Sleep(1);
+        //    //    }
+        //    //}
+        //    //).Start();
+
+        //    //wndModal wndModal = new wndModal();
+        //    //wndModal.Show();
+        }
+
 
         private void InitNetwork()
         {
@@ -610,14 +895,21 @@ namespace AquaExpert
         }
 
         bool on = false;
+        static DateTime dt0;
+        static TimeSpan ts;
+        static TimeSpan sum;
+
+        private Font font;
+        private Point p;
+
         private void timerTest_Tick(Gadgeteer.Timer timer)
         {
             timerTest.Stop();
 
             //DateTime dt = TimeManager.CurrentTime;
 
-            (busHubI2C.BusControlLines[1] as ControlLine).SetState(new byte[] { (byte)(on ? 1 : 0), 0 });
-            on = !on;
+            //(busHubI2C.BusControlLines[1] as ControlLine).SetState(new byte[] { (byte)(on ? 1 : 0), 0 });
+            //on = !on;
 
 
 
