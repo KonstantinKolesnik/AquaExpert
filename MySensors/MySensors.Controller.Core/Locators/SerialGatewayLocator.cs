73529e8f947f393843110d05f9581f88956da03e
Konstantin Kolesnik <gothicmaestro@live.com>
2014-10-23 01:52:23 +0300

    in progress

---
+++ MySensors/MySensors.Controller.Core/Locators/SerialGatewayLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySensors.Controller.Core.Locators
+{
+    public class SerialGatewayLocator
+    {
+
+    }
+}
