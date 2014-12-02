c8c3027f5b69cdaf65534e7d379ef2f34ca151ab
Konstantin Kolesnik <kk@techwire.dp.ua>
2014-12-02 13:48:23 +0200

    in progress

---
+++ MySensors/MySensors.Service/ControllerService.cs
@@ -0,0 +1,27 @@
+﻿using System.ServiceProcess;
+
+namespace MySensors.Service
+{
+    public partial class ControllerService : ServiceBase
+    {
+        public ControllerService()
+        {
+            InitializeComponent();
+
+            //HomeEnvironment.Init();
+
+            //app = new HomeApplication();
+            //app.Init();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //app.StartServices();
+        }
+
+        protected override void OnStop()
+        {
+            //app.StopServices();
+        }
+    }
+}
