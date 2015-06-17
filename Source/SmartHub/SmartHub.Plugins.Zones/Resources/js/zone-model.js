4b66eb53894ef7189f5daf62061318e3c73111a0
Konstantin Kolesnik <kk@techwire.dp.ua>
2015-06-17 19:27:11 +0300

    in progress

--- Source/SmartHub/SmartHub.Plugins.Zones/Resources/js/zone-model.js
+++ Source/SmartHub/SmartHub.Plugins.Zones/Resources/js/zone-model.js
@@ -37,28 +37,26 @@ define(['jquery'],
                 });
             },
             SignalRReceiveHandler: function (model, data) {
-                //setTimeout(function () {
-                    if (data.MsgId == "SensorValue") {
-                        data.Data.TimeStamp = new Date(data.Data.TimeStamp);
-                        var sv = data.Data;
+                if (data.MsgId == "SensorValue") {
+                    data.Data.TimeStamp = new Date(data.Data.TimeStamp);
+                    var sv = data.Data;
 
-                        if (model.Zone) {
-                            $.each(model.Zone.MonitorsList, function (idx, monitor) {
-                                if (monitor.Sensor.NodeNo == sv.NodeNo && monitor.Sensor.SensorNo == sv.SensorNo) {
-                                    monitor.Sensor.set("SensorValueValue", sv.Value);
-                                    monitor.Sensor.set("SensorValueTimeStamp", sv.TimeStamp);
+                    if (model.Zone) {
+                        $.each(model.Zone.MonitorsList, function (idx, monitor) {
+                            if (monitor.Sensor.NodeNo == sv.NodeNo && monitor.Sensor.SensorNo == sv.SensorNo) {
+                                monitor.Sensor.set("SensorValueValue", sv.Value);
+                                monitor.Sensor.set("SensorValueTimeStamp", sv.TimeStamp);
 
-                                    monitor.SensorValues.splice(0, 1);
-                                    monitor.SensorValues.push(sv);
-                                }
-                            });
+                                monitor.SensorValues.splice(0, 1);
+                                monitor.SensorValues.push(sv);
+                            }
+                        });
 
-                            $.each(model.Zone.ControllersList, function (idx, monitor) {
-                                // if ismymessage
-                            });
-                        }
+                        $.each(model.Zone.ControllersList, function (idx, monitor) {
+                            // if ismymessage
+                        });
                     }
-                //}, 0);
+                }
             }
         });
 
