6da0814a2c9c3e6469f21645cb1fae150b97d9d5
KonstantinKolesnik <gothicmaestro@live.com>
2016-10-10 18:14:45 +0300

    in progress

--- Source/SmartHub/SmartHub.UWP.Plugins.Audio/AudioPlugin.cs
+++ Source/SmartHub/SmartHub.UWP.Plugins.Audio/AudioPlugin.cs
@@ -6,59 +6,94 @@ using SmartHub.UWP.Core.Plugins;
 using System;
 using System.IO;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace SmartHub.UWP.Plugins.Audio
 {
-    public class AudioPlugin : PluginBase
+    public class AudioPlugin : PluginBase, IDisposable
     {
         #region Fields
+        private bool disposed = false;
         //private WaveOut waveOut;
-        private readonly object lockObject = new object();
-        private WasapiOutRT _audioOutput;
+        //private readonly object lockObject = new object();
+        private WasapiOutRT audioOutput;
         #endregion
 
         #region Plugin overrides
         public override void InitPlugin()
         {
             //waveOut = new WaveOut();
+            audioOutput = new WasapiOutRT(AudioClientShareMode.Shared, 200);
         }
         public override void StopPlugin()
         {
+            CloseAudioOutput();
             //waveOut.Dispose();
             //waveOut = null;
         }
         #endregion
 
-        #region API
+        #region Public methods
         //public /*async*/ IPlayback Play(Stream stream, int loop = 0)
-        //{
-        //    //lock (lockObject)
-        //    //{
-        //    //    var reader = new WaveFileReader(stream);
-        //    //    var loopStream = new LoopStream(/*Logger, */reader, loop);
+        //public IPlayback Play(Stream stream, int loop = 0)
+        public void Play(IRandomAccessStream stream)
+        {
+            //lock (lockObject)
+            //{
+            //    var reader = new WaveFileReader(stream);
+            //    var loopStream = new LoopStream(/*Logger, */reader, loop);
 
-        //    //    waveOut.Init(loopStream);
-        //    //    waveOut.Play();
+            //    waveOut.Init(loopStream);
+            //    waveOut.Play();
 
-        //    //    return loopStream;
-        //    //}
-
-
+            //    return loopStream;
+            //}
 
 
-        //    //IStorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(path, UriKind.Absolute));
-        //    //var s = await file.OpenAsync(FileAccessMode.Read);
 
-        //    //_audioOutput = new WasapiOutRT(AudioClientShareMode.Shared, 200);
-        //    //_audioOutput.Init(() =>
-        //    //{
-        //    //    var waveChannel32 = new WaveChannel32(new MediaFoundationReaderUniversal(s));
-        //    //    var mixer = new MixingSampleProvider(new ISampleProvider[] { waveChannel32.ToSampleProvider() });
-        //    //    return mixer.ToWaveProvider();
-        //    //});
-        //    //_audioOutput.Play();
-        //}
+
+            //IStorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(path, UriKind.Absolute));
+            //var s = await file.OpenAsync(FileAccessMode.Read);
+
+            //audioOutput.Init(() =>
+            //{
+            //    var waveChannel32 = new WaveChannel32(new MediaFoundationReaderUniversal(s));
+            //    var mixer = new MixingSampleProvider(new ISampleProvider[] { waveChannel32.ToSampleProvider() });
+            //    return mixer.ToWaveProvider();
+            //});
+
+            //audioOutput.Play();
+        }
+        public void Stop()
+        {
+            if (audioOutput != null)
+                audioOutput.Stop();
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
         #endregion
 
+        #region Private methods
+        private void CloseAudioOutput()
+        {
+            if (audioOutput != null)
+                audioOutput.Dispose();
+
+            audioOutput = null;
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                    CloseAudioOutput();
+
+                disposed = true;
+            }
+        }
+        #endregion
     }
 }
---
+++ Source/SmartHub/SmartHub.UWP.Plugins.Speech/VoiceCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SmartHub.UWP.Plugins.Speech
+{
+    public class VoiceCommand
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string CommandText { get; set; }
+        //public virtual UserScript UserScript { get; set; }
+    }
+}
