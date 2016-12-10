86c5cba49f221e4579dfcc30a4f3974ab125dd35
KonstantinKolesnik <gothicmaestro@live.com>
2016-12-10 04:19:24 +0200

    in progress

--- Source/SmartHub/Wemos.GenericNode/Wemos.GenericNode.ino
+++ Source/SmartHub/Wemos.GenericNode/Wemos.GenericNode.ino
@@ -1,12 +1,13 @@
 #include <WemosNode.h>
 #include <WemosLine.h>
+#include <TimeLib.h>
 
-ADC_MODE(ADC_VCC);
-WemosNode node(5);
+//ADC_MODE(ADC_VCC);
+WemosNode node(0);
 
-#define LAST_MS	-100000
-unsigned long prevMs = LAST_MS;
-const unsigned long interval = 2000;
+#define LAST_MS	0//-100000
+unsigned long prevMsTime = LAST_MS;
+const unsigned long intervalTime = 1000;
 
 void setup()
 {
@@ -21,8 +22,35 @@ void loop()
 {
 	node.process();
 
-	//if (node.hasIntervalElapsed(&prevMs, interval))
-	//{
+	if (node.hasIntervalElapsed(&prevMsTime, intervalTime))
+	{
+		String s = node.getTimeString();
+		//if (!s.equals(""))
+		if (s != "")
+			Serial.println();
 
-	//}
+		//Serial.print(hour());
+		//printDigits(minute());
+		//printDigits(second());
+
+		//Serial.print(" ");
+		//Serial.print(day());
+
+		//Serial.print("-");
+		//Serial.print(month());
+
+		//Serial.print("-");
+		//Serial.print(year());
+
+		//Serial.println();
+	}
+}
+
+void printDigits(int digits)
+{
+	// utility function for digital clock display: prints preceding colon and leading 0
+	Serial.print(":");
+	if (digits < 10)
+		Serial.print('0');
+	Serial.print(digits);
 }
