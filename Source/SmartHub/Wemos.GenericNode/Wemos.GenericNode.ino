dd677d0b67a0688eeab45f8c1536c1426eae1a3b
KonstantinKolesnik <gothicmaestro@live.com>
2016-12-11 03:16:03 +0200

    in progress

--- Source/SmartHub/Wemos.GenericNode/Wemos.GenericNode.ino
+++ Source/SmartHub/Wemos.GenericNode/Wemos.GenericNode.ino
@@ -1,9 +1,9 @@
 #include <WemosNode.h>
-#include <WemosLine.h>
+#include <WemosModuleRelay.h>
 #include <SFE_MicroOLED.h>
 
 //ADC_MODE(ADC_VCC);
-WemosNode node(0);
+WemosNode node(1);
 
 #define LAST_MS	0//-100000
 unsigned long prevMsTime = LAST_MS;
@@ -15,22 +15,21 @@ MicroOLED oled(PIN_RESET, DC_JUMPER); // Example I2C declaration
 
 void setup()
 {
-	//node.addLine();
-	//node.addLine();
-	//node.addLine();
+	node.addModule(new WemosModuleRelay(0, "Relay 0", D1, true));
+	//node.addModule();
+	//node.addModule();
 
 	node.begin();
 
-	node.sendFirmwareName("Test Node");
-	node.sendFirmwareVersion(1.0);
+	node.sendFirmwareInfo("Test Node", 1.0);
 
-	oled.begin();
-	oled.clear(ALL);
-	oled.setFontType(0);  // Set the text to small (10 columns, 6 rows worth of characters).
-	//oled.setFontType(1);  // Set the text to medium (6 columns, 3 rows worth of characters).
-	//oled.setFontType(2);  // Set the text to medium/7-segment (5 columns, 3 rows worth of characters).
-	//oled.setFontType(3);  // Set the text to large (5 columns, 1 row worth of characters).
-	oled.display();
+	//oled.begin();
+	//oled.clear(ALL);
+	//oled.setFontType(0);  // Set the text to small (10 columns, 6 rows worth of characters).
+	////oled.setFontType(1);  // Set the text to medium (6 columns, 3 rows worth of characters).
+	////oled.setFontType(2);  // Set the text to medium/7-segment (5 columns, 3 rows worth of characters).
+	////oled.setFontType(3);  // Set the text to large (5 columns, 1 row worth of characters).
+	//oled.display();
 }
 
 void loop()
@@ -39,16 +38,16 @@ void loop()
 
 	if (node.hasIntervalElapsed(&prevMsTime, intervalTime))
 	{
-		String t = "", d = "";
-		if (node.getDateTimeString(d, t))
-		{
-			//Serial.println(d +" " + t);
+		//String t = "", d = "";
+		//if (node.getDateTimeString(d, t))
+		//{
+		//	//Serial.println(d +" " + t);
 
-			oled.clear(PAGE);
-			oled.setCursor(0, 0);
-			oled.println(d);
-			oled.println(t);
-			oled.display();
-		}
+		//	oled.clear(PAGE);
+		//	oled.setCursor(0, 0);
+		//	oled.println(d);
+		//	oled.println(t);
+		//	oled.display();
+		//}
 	}
 }
